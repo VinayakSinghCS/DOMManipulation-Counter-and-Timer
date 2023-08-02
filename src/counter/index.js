fffa4cd8036bbe67c1f5c756d6c26d45989a5c8b
VinayakSinghCS <vinayak.singh@contentstack.com>
2023-08-02 10:47:33 +0530

    binding changes

--- src/counter/index.js
+++ src/counter/index.js
@@ -2,62 +2,61 @@
 class Counter{
     constructor(){
         this.count = 0;
-        
+        this.counterValue = null;
     }
 
-    increment(){
+    increment(component){
+        console.log("inside increment");
         this.count++;
+        this.updateDOM()
     }
 
     decrement(){
         this.count--;
+        this.updateDOM();
     }
 
     reset(){
         this.count = 0;
+        this.updateDOM();
+    }
+
+    updateDOM(component){
+        this.counterValue.innerText = this.count;
     }
 
     render(){
         // create elements
         const counterComponent = document.createElement("div");
-        const counterValue = document.createElement("p");
+        this.counterValue = document.createElement("p");
         const incrementButton = document.createElement("button");
         const decrementButton = document.createElement("button");
         const resetButton = document.createElement("button");
 
 
         //initial value of the component
-        counterValue.innerText = '0';
+        this.counterValue.innerText = '0';
         incrementButton.innerText = '+';
         decrementButton.innerText = '-';
         resetButton.innerText = 'reset';
     
         // give every element id
         counterComponent.classList.add('counter-component');
-        counterValue.classList.add('counter-value');
+        this.counterValue.classList.add('counter-value');
         incrementButton.classList.add('incrementButton');
         decrementButton.classList.add('decrementButton');
         resetButton.classList.add('resetButton');
 
         // add event listener (should append unique id for every instance)
-        incrementButton.addEventListener('click',()=>{
-            this.increment();
-            counterValue.innerText = this.count;
-        })
+        incrementButton.addEventListener('click',this.increment.bind(this))
 
-        decrementButton.addEventListener('click',()=>{
-            this.decrement();
-            counterValue.innerText = this.count;
-        })
+        decrementButton.addEventListener('click',this.decrement.bind(this))
 
-        resetButton.addEventListener('click',()=>{
-            this.reset();
-            counterValue.innerText = this.count;
-        })
+        resetButton.addEventListener('click',this.reset.bind(this))
 
         // append child to parent 
 
-        counterComponent.appendChild(counterValue);
+        counterComponent.appendChild(this.counterValue);
         counterComponent.appendChild(incrementButton);
         counterComponent.appendChild(decrementButton);
         counterComponent.appendChild(resetButton);
